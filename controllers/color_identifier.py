6cfc959a911cecd50ab455150cd7be3edced5a80
Minh Ta Anh <taanhminh2000@gmail.com>
2023-03-04 17:25:11 -0500

    added feature to more accurately identify color

--- controllers/color_identifier.py
+++ controllers/color_identifier.py
@@ -1,3 +1,5 @@
+from scipy.spatial import KDTree
+from webcolors import CSS3_HEX_TO_NAMES, hex_to_rgb
 def detect_properties(path):
     """Detects image properties in the file."""
     from google.cloud import vision
@@ -12,16 +14,34 @@ def detect_properties(path):
     response = client.image_properties(image=image)
     props = response.image_properties_annotation
     print('Properties:')
-
+    
+    arr = []
+    iteration = 0
     for color in props.dominant_colors.colors:
-        print('fraction: {}'.format(color.pixel_fraction))
-        print('\tr: {}'.format(color.color.red))
-        print('\tg: {}'.format(color.color.green))
-        print('\tb: {}'.format(color.color.blue))
-        print('\ta: {}'.format(color.color.alpha))
+        arr.append([])
+        arr[iteration].append(color.pixel_fraction)
+        arr[iteration].append(convert_rgb_to_names((color.color.red, color.color.green, color.color.blue)))
+        iteration = iteration + 1
+        
+    print(arr)
 
     if response.error.message:
         raise Exception(
             '{}\nFor more info on error messages, check: '
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
+    return arr
+
+def convert_rgb_to_names(rgb_tuple):
+    
+    # a dictionary of all the hex and their respective names in css3
+    css3_db = CSS3_HEX_TO_NAMES
+    names = []
+    rgb_values = []
+    for color_hex, color_name in css3_db.items():
+        names.append(color_name)
+        rgb_values.append(hex_to_rgb(color_hex))
+    
+    kdt_db = KDTree(rgb_values)
+    distance, index = kdt_db.query(rgb_tuple)
+    return f'closest match: {names[index]}'
