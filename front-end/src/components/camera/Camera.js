ce0722a7557ea6f9611b324b8d88293903e52c26
QuinnVo1911 <khanhquynh.havo@gmail.com>
2023-03-04 17:43:52 -0500

    remove node modules

---
+++ front-end/src/components/camera/Camera.js
@@ -0,0 +1,136 @@
+import React, {useRef, useState, useEffect} from "react";
+import styles from "./styles.module.css";
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import { textToSpeech } from "../../javascript/textToSpeech";
+
+const object = {
+   url: 'https://static.wikia.nocookie.net/mana/images/7/71/SD3Poto.png/revision/latest/scale-to-width-down/310?cb=20120108212923',
+   objects: ['apple', 'banana', 'shat']
+}
+const Camera = () => {
+   const videoRef = useRef(null);
+   const photoRef = useRef(null);
+
+   const [hasPhoto, setHasPhoto] = useState(false);
+   const [isProcessingPhoto, setProcessingPhoto] = useState(false);
+   const [data, setData] = useState({
+      url: '',
+      objects: []
+   });
+   // const [message, setMessage] = useState({
+   //    error: "Sorry I cannot find any object. Please try again.",
+   //    success: "I found"
+   // })
+
+   const getVideo = () => {
+      navigator.mediaDevices
+         .getUserMedia(
+            {video : {width: 1920, height: 1080}
+         })
+         .then(stream => {
+            let video = videoRef.current;
+            video.srcObject = stream;
+            video.play();
+         })
+         .catch(err => {
+            console.error(err);
+         })
+         
+   }
+
+   const takePhoto = () => {
+      const width = 414;
+      const height = width / (16/9);
+
+      let video = videoRef.current;
+      let photo = photoRef.current;
+
+      photo.width = width;
+      photo.height = height;
+
+      let ctx = photo.getContext('2d');
+      ctx.drawImage(video, 0, 0, width, height);
+      console.log(ctx);
+      console.log(photoRef.current);
+      //Save image 
+      // canvasToImage(photoRef.current);
+      //send url for backend, if get object.length==0 -> sorry; if have object -> call textToSpeech
+      setImageUrl(photoRef.current.toDataURL('../image/'));
+      postUrlAPI();
+      getUrlAPI();
+
+      setHasPhoto(true); 
+   }
+
+   const postUrlAPI = async() => {
+      const response = await fetch('/api/image', {
+         method: 'POST',
+         headers: {
+           'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({ url: data.imageUrl})
+       })
+       .then(response => response.json())
+       .then(data => {
+         console.log(data);
+       })
+       .catch(error => {
+         console.error(error);
+       });
+   }
+
+   const getUrlAPI = async() => {
+      setProcessingPhoto(true);
+      const result = await response.json();
+      await textToSpeech(result)
+      if (result.objects.length > 0){
+         setData({ url: result.url, objects: result.objects, ...data});
+      } else {
+      }
+      setProcessingPhoto(false);
+   }
+
+
+   const closePhoto = () => {
+      let photo = photoRef.current;
+      let ctx = photo.getContext('2d');
+
+      ctx.clearRect(0, 0, photo.width, photo.height);
+
+      setHasPhoto(false); 
+   }
+
+   useEffect(() => {
+      getVideo();
+   }, [videoRef])
+
+   return (
+      <div className={styles.wrapper}>
+         <div className={styles.camera}>
+            <video ref={videoRef}></video>
+            <button onClick={takePhoto}>Take a picture!</button>
+         </div>
+         <div className={hasPhoto? styles.resultHasPhoto : styles.result}>
+            {isProcessingPhoto ? 
+               <div> 
+                  <CircularProgress /> 
+                  <canvas ref={photoRef}></canvas>
+                  <GraphicEqIcon /><h3>Processing image...</h3>
+               </div>
+               : 
+               <div> 
+                  <img src={data.url} alt="My Image" />
+                  {(async () => await textToSpeech(data.objects))()}
+                  <GraphicEqIcon />
+               </div>
+            
+            }
+            
+         </div>
+      </div>
+   )
+}
+
+export default Camera;
--- front-end/src/javascript/textToSpeech.js
+++ front-end/src/javascript/textToSpeech.js
@@ -4,19 +4,16 @@ const textToSpeech = require('@google-cloud/text-to-speech');
 // Import other required libraries
 const fs = require('fs');
 const util = require('util');
-const data = require('./text.json');
-// fetch ('C:\Users\taanh\OneDrive\Desktop\React learning\test Google API\text.json').then(response => response.json()).then(data => console.log(data)).catch(error => console.log(error));
-console.log(data);
-const database = data.object;
+
 // Creates a client
 const client = new textToSpeech.TextToSpeechClient();
-async function quickStart(database) {
+export async function textToSpeech(data) {
   // Construct the request
   let text = "";
   if (data.length == 0){
     text = "Sorry, i cannot find any object. Please try again.";
   }else{
-    const temp = database.join(", ");
+    const temp = data.join(", ");
     text = `I found ${temp} in your image.`;
   }
   const request = {
@@ -35,4 +32,4 @@ async function quickStart(database) {
   await writeFile('output.mp3', response.audioContent, 'binary');
   console.log('Audio content written to file: output.mp3');
 }
-quickStart(database);
+textToSpeech(data);
